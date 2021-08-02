380db088a5a4a236816b8d7e28f0c516b2332f31
terrisanti <86049150+terrisanti@users.noreply.github.com>
2021-08-03 03:53:12 +0800

    Added apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://17c4983e.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
